63f5c72aad990196141911ca3cf26403138013c7
RazorHex <ahda043@gmail.com>
2022-11-11 18:53:19 +0700

    First Commit

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 
-app.get('/', (request, response) => response.send('Hello World'))
+app.get('/', (request, response) => {
+  response.sendFile(path.join(__dirname, 'hello_world.html'))
+})
 app.listen(9000)
 
 console.log('Online')
