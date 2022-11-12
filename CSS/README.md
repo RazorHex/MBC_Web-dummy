c0abde31a393692007f4165c81a97634e779170d
RazorHex <ahda043@gmail.com>
2022-11-13 01:02:41 +0700

    First Commit

--- CSS/README.md
+++ CSS/README.md
@@ -1,1 +1,4 @@
 # Understanding [CSS](https://www.w3schools.com/css/)
+
+# Documentations
+1. [Text Styling](https://youtu.be/azZCW24XtT4)
--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-app.get('/', (request, response) => {
-  response.sendFile(path.join(__dirname, 'hello_world.html'))
-})
-app.listen(9000)
+app.use(express.static(path.join(__dirname, '/')))
 
-console.log('Online')
+app.get('/', (request, response) => {
+  response.sendFile(path.join(__dirname, 'HTML/home.html'))
+})
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'HTML/about.html'))
+})
+
+app.listen(9000, () => console.log('Listening on port 9000'))
