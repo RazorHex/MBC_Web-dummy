29aca7da71844bb2ecdb3576a0ec46fd78b120e2
RazorHex <ahda043@gmail.com>
2022-11-21 20:39:38 +0700

    Adding search bar on home.html, simplify get method on index.js, and on progress program.js for utilizing the search

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
 const express = require('express')
 const path = require('path')
+const fs = require('fs')
+const s = require('./js/program')
 const app = express()
 const port = 9000
 
 app.use(express.static(path.join(__dirname, '/')))
+app.use(express.urlencoded({
+  extended: true
+}))
 
-app.get('/', (request, response) => {
-  response.sendFile(path.join(__dirname, 'HTML/home.html'))
-})
+const files = fs.readdirSync(path.join(__dirname, 'HTML'))
 
-app.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname, 'HTML/about.html'))
+// using method get to all the html files
+for(f in files) {
+  const file = files[f]
+  if(!file.endsWith('.html')) {
+    continue
+  }
+
+  if(file == 'home.html') {
+    app.get('/', (req, res) => {
+      res.sendFile(path.join(__dirname, `HTML/${file}`))
+    })
+  } else {
+    app.get(`/${file.split('.')[0]}`, (req, res) => {
+      res.sendFile(path.join(__dirname, `HTML/${file}`))
+    })
+  }
+}
+
+app.post('/search', (req, res) => {
+  res.redirect('/')
+  s.search(req.body.search)
 })
 
 app.listen(port, () => console.log('Listening on port 9000'))
